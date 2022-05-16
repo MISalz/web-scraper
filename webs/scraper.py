46d8e8ad35330a641c02380981fe07041ecb7257
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-05-15 20:53:07 -0700

    webscraper

--- webs/scraper.py
+++ webs/scraper.py
@@ -1,7 +1,34 @@
+import requests
+import re
+from bs4 import BeautifulSoup
 
-def get_citations_needed_count():
-  pass
+URL = "https://en.wikipedia.org/wiki/Bavaria/"
+page = requests.get(URL)
+
+def get_citations_needed_count(url):
+    citation_count = 0
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    citation_needed = soup.find_all(title='Wikipedia:Citation needed')
+    for needed in citation_needed:
+        citation_count += 1
+
+    print(f'** The Wikipedia page at url: {url} needs {citation_count} citations **')
 
 
-def get_citations_needed_report():
-  pass
+
+def get_citations_needed_report(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    citation_needed = soup.find_all(title='Wikipedia:Citation needed')
+    print("** The following lines require citation **")
+    for needed in citation_needed:
+        content = str(needed.parent.parent.parent)
+        pattern = r"<[^<>]+>"
+        parsed_content = re.sub(pattern, '', content)
+        print(parsed_content)
+
+
+get_citations_needed_count(URL)
+
+get_citations_needed_report(URL)
